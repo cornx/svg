884bdea025ea0076008e2c66a79e56b3a5857aa6
cornx <exit>
2016-11-04 15:40:35 +0100

    svg testiranje

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
- var fajl = "img/plodine.svg";
- new Vivus('pleter', {
-     file: "img/pleter.svg",
-     duration: 5000,
- }, false);
- var start = function() {
-     document.getElementById("pleter").remove();
-     new Vivus('plodine', {
-         file: fajl,
-         duration: 200,
-     }, false);
-     new Vivus('ime', {
-         file: "img/ime.svg",
-         duration: 50,
-     }, false);
- };
- var apt = function() {
-     document.getElementById("pleter").remove();
-     new Vivus('apt', {
-         file: "img/apt_proboj.svg",
-         duration: 200,
-         type: "oneByOne"
-     });
- };
+document.addEventListener("DOMContentLoaded", function (event) {
+    console.log("DOM fully loaded and parsed");
+});
+
+    var fajl = "img/plodine.svg";
+    // klikni za prikaz  
+    var start = function() {
+        document.getElementById("pleter").remove();
+        new Vivus('plodine', {
+            file: fajl,
+            duration: 200,
+        }, false);
+        new Vivus('ime', {
+            file: "img/ime.svg",
+            duration: 50,
+        }, false);
+    };
+
+    // klikni za prikaz apartmana
+    var apt = function() {
+        document.getElementById("pleter").remove();
+        new Vivus('apt', {
+            file: "img/apt_proboj.svg",
+            duration: 200,
+            type: "oneByOne"
+        });
+    };
+
+
+    new Vivus('pleter', {
+        file: "img/pleter.svg",
+        duration: 5000,
+    }, false);
