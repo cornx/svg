acc77cae1251fa4240578fe8112720aa1a6aadf9
cornx <exit>
2016-11-04 18:54:22 +0100

    dodan labirint

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
     var aptBtn = document.getElementById("apartmani"),
-        prikaziBtn = document.getElementById("pokazi"),
+        plodineBtn = document.getElementById("plodine"),
         pleterBtn = document.getElementById("pleter"),
         danijelBtn = document.getElementById("danijel"),
+        labirintBtn = document.getElementById("labirint"),
+        labirintBtnRjesenje = document.getElementById("labirint-rjesenje"),
         main = document.querySelector("#main");
 
     // klikni za prikaz  
@@ -23,12 +25,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     });
 
 
-    prikaziBtn.addEventListener("click", function() {
+    plodineBtn.addEventListener("click", function() {
         // main.innerHTML = "";
 
         while (main.firstChild) {
             main.removeChild(main.firstChild);
-            main.classList += "plodine";
+            main.classList = "plodine";
         };
 
         new Vivus(main, {
@@ -40,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // klikni za prikaz apartmana
     aptBtn.addEventListener("click", function() {
         // main.innerHTML = "";
-         while (main.firstChild) {
+        while (main.firstChild) {
             main.removeChild(main.firstChild);
             main.classList = "";
         };
@@ -50,16 +52,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
             type: "oneByOne"
         });
     });
-    
-        danijelBtn.addEventListener("click", function() {
+
+    danijelBtn.addEventListener("click", function() {
         // main.innerHTML = "";
-         while (main.firstChild) {
+        while (main.firstChild) {
             main.removeChild(main.firstChild);
             main.classList = "";
         };
         new Vivus(main, {
             file: "img/ime.svg",
             duration: 200,
+            /*type: "oneByOne"*/
+        });
+    });
+    labirintBtn.addEventListener("click", function() {
+        // main.innerHTML = "";
+        while (main.firstChild) {
+            main.removeChild(main.firstChild);
+            main.classList = "labirint";
+        };
+        new Vivus(main, {
+            file: "img/labirint.svg",
+            duration: 100,
+            /*type: "oneByOne"*/
+        });
+    });
+    labirintBtnRjesenje.addEventListener("click", function() {
+        // main.innerHTML = "";
+        while (main.firstChild) {
+            main.removeChild(main.firstChild);
+            main.classList = "labirint-rjesenje";
+        };
+        new Vivus(main, {
+            file: "img/labirint-rjesenje.svg",
+            duration: 400,
             /*type: "oneByOne"*/
         });
     });
