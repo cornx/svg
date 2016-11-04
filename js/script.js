50c7b870304b8a9e088914076f0b4acfbbff554a
cornx <exit>
2016-11-04 22:45:10 +0100

    dodan keybinder

--- js/script.js
+++ js/script.js
@@ -89,4 +89,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
             /*type: "oneByOne"*/
         });
     });
+    
+var i = document.body.style;
+
+keyboardJS.bind('a', function(e) {i.background = "orange";});
+keyboardJS.bind('v', function(e) {i.background = "red";});
+keyboardJS.bind('s', function(e) {i.background = "blue";});
+keyboardJS.bind('r', function(e) {i.background = "pink";});
+keyboardJS.bind('d', function(e) {i.background = "grey";});
+keyboardJS.bind('f', function(e) {i.background = "black";});
+keyboardJS.bind('g', function(e) {i.background = "green";});
+    
+    
 });
+
