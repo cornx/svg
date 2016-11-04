aabcb784735f2cec5b53f8c68065b304c630722f
cornx <exit>
2016-11-04 15:25:36 +0100

    svg testiranje

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
- var fajl = "../img/plodine.svg";
+ var fajl = "img/plodine.svg";
  new Vivus('pleter', {
-     file: "../img/pleter.svg",
+     file: "img/pleter.svg",
      duration: 5000,
  }, false);
  var start = function() {
@@ -10,14 +10,14 @@
          duration: 200,
      }, false);
      new Vivus('ime', {
-         file: "../img/ime.svg",
+         file: "img/ime.svg",
          duration: 50,
      }, false);
  };
  var apt = function() {
      document.getElementById("pleter").remove();
      new Vivus('apt', {
-         file: "../img/apt_proboj.svg",
+         file: "img/apt_proboj.svg",
          duration: 200,
          type: "oneByOne"
      });
